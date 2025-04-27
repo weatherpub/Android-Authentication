d23048533f1192df4b6c0d79f159709b0cf30b37
Patrick <binary_thinker@yahoo.com>
2025-04-27 14:56:26 -0700

    Latest Trades Model

---
+++ app/src/main/java/edu/sfsu/authentication/model/home/LatestTradesModel.java
@@ -0,0 +1,55 @@
+package edu.sfsu.authentication.model.home;
+
+public class LatestTradesModel {
+    private String trades;
+    private String c;
+    private String i;
+    private String p;
+    private String s;
+    private String t;
+    private String x;
+    private String z;
+
+    public LatestTradesModel(String trades, String c, String i, String p, String s, String t, String x, String z) {
+        this.trades = trades;
+        this.c = c;
+        this.i = i;
+        this.p = p;
+        this.s = s;
+        this.t = t;
+        this.x = x;
+        this.z = z;
+    }
+
+    public String getTrades() {
+        return trades;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public String getI() {
+        return i;
+    }
+
+    public String getP() {
+        return p;
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public String getT() {
+        return t;
+    }
+
+    public String getX() {
+        return x;
+    }
+
+    public String getZ() {
+        return z;
+    }
+}
