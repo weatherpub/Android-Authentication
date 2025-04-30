c7688eb11af3c3afd425c08b8109940b402ec150
Patrick <binary_thinker@yahoo.com>
2025-04-30 13:38:49 -0700

    Singleton file for DrinkViewModel

---
+++ app/src/main/java/edu/sfsu/authentication/vm/DrinkViewModel.java
@@ -0,0 +1,23 @@
+package edu.sfsu.authentication.vm;
+
+import java.util.ArrayList;
+
+import edu.sfsu.authentication.model.home.DrinkModel;
+
+public class DrinkViewModel {
+    private static final DrinkViewModel drinksViewModel = new DrinkViewModel();
+
+    private final ArrayList<DrinkModel> model;
+
+    public static DrinkViewModel getInstance() {
+        return drinksViewModel;
+    }
+
+    private DrinkViewModel() {
+        model = new ArrayList<>();
+    }
+
+    public ArrayList<DrinkModel> getModel() {
+        return model;
+    }
+}
