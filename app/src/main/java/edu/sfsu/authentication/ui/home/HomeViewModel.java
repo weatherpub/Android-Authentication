e9ca76ae1c8d2b47985c8bae38be1965f2fe2aaf
Patrick <binary_thinker@yahoo.com>
2025-05-05 19:02:46 -0700

    A very thin ViewModel (Thumbs Up)

--- app/src/main/java/edu/sfsu/authentication/ui/home/HomeViewModel.java
+++ app/src/main/java/edu/sfsu/authentication/ui/home/HomeViewModel.java
@@ -1,29 +1,13 @@
 package edu.sfsu.authentication.ui.home;
-
-import android.os.AsyncTask;
-import android.util.Log;
-
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.util.ArrayList;
 
 import edu.sfsu.authentication.async.HomeViewAsyncTask;
 import edu.sfsu.authentication.model.home.DrinkModel;
-import edu.sfsu.authentication.vm.DrinkViewModel;
-import okhttp3.OkHttpClient;
-
-import okhttp3.Request;
-import okhttp3.Response;
-
 public class HomeViewModel extends ViewModel {
 
-    // MutableLiveData<ArrayList<DrinkModel>> is also used in HomeFragment.
+    // HomeFragment calls this method
     public MutableLiveData<ArrayList<DrinkModel>> getMutableLiveData() {
         return new MutableLiveData<>();
     }
