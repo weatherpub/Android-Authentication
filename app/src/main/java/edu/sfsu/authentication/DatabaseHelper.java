339f7f0b4e55efab9ccfbe038cf144b599f3f19b
Patrick <binary_thinker@yahoo.com>
2025-05-17 12:40:57 -0700

    Implementing the Database Infrastructure

--- app/src/main/java/edu/sfsu/authentication/DatabaseHelper.java
+++ app/src/main/java/edu/sfsu/authentication/DatabaseHelper.java
@@ -5,42 +5,60 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
-
+// Inter Fragment Communication - Android Programming for Beginners Book page 427
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private static final String DB_NAME = "automobile";
-    private static final int DB_VERSION = 1; // upgrade or downgrade db version
+    /**
+     * DATABASE => UPPERCASE, Table => Capitalized
+     */
+    // using constant fields
+    private static final String DB_NAME = "AUTOMOBILE"; // capitalize db name, lowercase table name
+    private static final int DB_VERSION = 1; // version options 0 - 3
+    private static final int USER_VERSION = 0; // version options 0 - 3
 
-    DatabaseHelper(Context context) {
-        super(context, DB_NAME, null, DB_VERSION);
+    // we can access the string outside the class as well because it's declared as 'public'.
+    public static final String TABLE_ROW_ID = "_id";
+    public static final String TABLE_ROW_MAKE = "make";
+    public static final String TABLE_ROW_DESCRIPTION = "description";
+
+    /**
+     * Public Constructor
+     * SQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version)
+     */
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION); // options
     }
 
-    private static void alter_table(SQLiteDatabase db) {
-        db.execSQL("ALTER TABLE car ADD COLUMN make TEXT");
+    // create a new motorcycle table for downgrade db
+    private static void create_new_table(SQLiteDatabase db) {
+        Log.i("log", "create_table(SQLiteDatabase db)");
+        db.execSQL("CREATE TABLE Motorcycle(TABLE_ROW_ID integer PRIMARY KEY AUTOINCREMENT, "
+                + TABLE_ROW_MAKE + "text, "
+                + TABLE_ROW_DESCRIPTION + "description text, "
+                + "image_resource_id integer);");
+        // change to motorcycle models and change the descriptions
+        insert_item(db, "Harley Davidson", "Chevrolet is an American automotive company.", R.drawable.camaro);
+        insert_item(db, "Kawasaki", "Chrysler is an American Automotive Company", R.drawable.chevelle);
+        insert_item(db, "Yamaha", "Ford is an American Automotive Company.", R.drawable.mustang);
     }
 
+    // Default Method
     private static void create_table(SQLiteDatabase db) {
         Log.i("log", "create_table(SQLiteDatabase db)");
-        db.execSQL("CREATE TABLE car(_id integer PRIMARY KEY AUTOINCREMENT, "
+        db.execSQL("CREATE TABLE Car(_id integer PRIMARY KEY AUTOINCREMENT, "
                 + "make text, "
                 + "description text, "
                 + "image_resource_id integer);");
-        insert_item(db, "Chevrolet", "Chevrolet is an American automotive company.", R.drawable.Chevelle);
-        insert_item(db, "Chrysler", "Chrysler is an American Automotive Company", R.drawable.Mustang);
-        insert_item(db, "Ford", "Ford is an American Automotive Company.", R.drawable.Camary);
+        insert_item(db, "Chevrolet", "Chevrolet is an American automotive company.", R.drawable.camaro);
+        insert_item(db, "Chrysler", "Chrysler is an American Automotive Company", R.drawable.chevelle);
+        insert_item(db, "Ford", "Ford is an American Automotive Company.", R.drawable.mustang);
     }
 
     // DELETE a record in the car table.
     public static void delete_record(SQLiteDatabase db) {
-
+        Log.i("log", "delete_record(SQLiteDatabase db)");
     }
 
-    // This should update the table, if I update the DB_VERSION to 2
-    public static void update_record(SQLiteDatabase db) { // UPDATE a record in the car table.
-        ContentValues UpdateValues = new ContentValues();
-        UpdateValues.put("description", "Ford is a Fortune 500 Company and I own stock");
-        db.update("car", UpdateValues, "make = ?", new String[] {"Ford"});
-    }
-
+    // Insert an item into the table
     private static void insert_item(SQLiteDatabase db,
                                     String make,
                                     String description,
@@ -54,27 +72,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         db.insert("car", null, ItemValues);
     }
 
-    private void update_database(SQLiteDatabase db, int user_version, int new_version) {
-        /*
-        if(user_version == 0) { // if version of db is 0
-            Log.i("log", "Database Connection is");
-            db.execSQL("CREATE TABLE car(_id integer PRIMARY KEY AUTOINCREMENT, "
-                    + "make text, "
-                    + "description text, "
-                    + "image_resource_id integer);");
-            insert_item(db, "Chevrolet", "Chevrolet is an American automotive company.", R.drawable.Chevelle);
-            insert_item(db, "Chrysler", "Chrysler is an American Automotive Company", R.drawable.Mustang);
-            insert_item(db, "Ford", "Ford is an American Automotive Company.", R.drawable.Camary);
-        } else if(user_version == 1) {
-            Log.i("log", "user_version = 1, update_records()");
-            update_record(db); // update a record in the table
-        } else if(user_version == 2) {
-            delete_record(db);
-        } else if(user_version == 3) {
-            db.execSQL("ALTER TABLE car ADD COLUMN make TEXT");
-        }
-        */
+    // This should update the table, if I update the DB_VERSION to 2
+    public static void update_record(SQLiteDatabase db) { // UPDATE a record in the car table.
+        ContentValues UpdateValues = new ContentValues();
+        UpdateValues.put("description", "Ford is a Fortune 500 Company and I own stock");
+        db.update("car", UpdateValues, "make = ?", new String[] {"Ford"});
+    }
 
+    // Update the DB using a switch statement
+    private void update_database(SQLiteDatabase db, int user_version, int new_version) {
         switch (user_version) {
             case 0: create_table(db);
                 break;
@@ -82,18 +88,35 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 break;
             case 2: delete_record(db);
                 break;
-            case 3: alter_table(db);
+            case 3: db.execSQL("ALTER TABLE car ADD COLUMN make TEXT");
                 break;
+            default:
+                Log.i("log", "user_version default selection");
         }
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) { // mandatory
-        update_database(sqLiteDatabase, 0, DB_VERSION);
+        Log.i("log", "Opening Database: " + getDatabaseName());
+        update_database(sqLiteDatabase, USER_VERSION, DB_VERSION);
     }
 
+    /**
+     * If the sqlite helper spots that the database that's installed is out of date,
+     * it will call either the onUpgrade() or onDowngrade() method.
+     */
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) { // mandatory
-        update_database(sqLiteDatabase, oldVersion, newVersion);
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int user_version, int new_version) { // mandatory
+        update_database(sqLiteDatabase, user_version, new_version);
+    }
+
+    // called when the database needs to be downgraded.
+    public void onDowngrade(SQLiteDatabase db, int old_version, int new_version) {
+        // if the old version is 1, then we're going to revert the database to version 0
+        if (old_version == 1) {
+            create_table(db);
+        } else if (old_version == 2) {
+            create_new_table(db);
+        }
     }
 }
--- app/src/main/java/edu/sfsu/authentication/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/edu/sfsu/authentication/ui/notifications/NotificationsFragment.java
@@ -1,32 +1,72 @@
 package edu.sfsu.authentication.ui.notifications;
 
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.w3c.dom.Text;
+
+import edu.sfsu.authentication.DatabaseHelper;
+import edu.sfsu.authentication.R;
 import edu.sfsu.authentication.databinding.FragmentNotificationsBinding;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Get a reference to the database.
+        SQLiteOpenHelper databaseHelper = new DatabaseHelper(getContext());
+        try {
+            SQLiteDatabase db = databaseHelper.getReadableDatabase();
+
+            // Create a Cursor and get a reference to it. Return all records from a table.
+            Cursor cursor = db.query("Car", new String[] {"_id", "make", "description"}, null, null, null, null, null);
+
+            if(cursor.moveToFirst()) {
+                String make_txt = cursor.getString(0);
+                String description_txt = cursor.getString(1);
+                String id_txt = cursor.getString(2);
+
+                TextView make = (TextView)getActivity().findViewById(R.id.tv_make);
+                make.setText(make_txt);
+
+                TextView description = (TextView)getActivity().findViewById(R.id.tv_description);
+                description.setText(description_txt);
+
+                TextView id = (TextView)getActivity().findViewById(R.id.tv_record);
+                id.setText(id_txt);
+            }
+            cursor.close();
+            db.close();
+        } catch (SQLException e) {
+            Toast toast = Toast.makeText(getContext(), "Database Unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        NotificationsViewModel notificationsViewModel = new ViewModelProvider(this).get(NotificationsViewModel.class);
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        View view = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        /* *
+        * Final TextView textView = binding.textNotifications;
+        * notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        */
+
+        return view ;
     }
 
     @Override
